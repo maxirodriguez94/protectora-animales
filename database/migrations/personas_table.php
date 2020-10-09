c87a3a4902926d7eb4f6aa692f6cf16ca5b71074
Bruno-Vergara <vergarabruno19@gmail.com>
2020-10-09 19:40:26 -0300

    cree la entidad persona

---
+++ database/migrations/personas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Personas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('dni')->unique();
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('localidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
