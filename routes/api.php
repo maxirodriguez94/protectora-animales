719020db8907e1c3428a084a8d54f27a711a5184
maxi <maxi.rodriguez94@gmail.com>
2020-10-17 14:26:13 -0300

    Borrar use no usado

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,7 @@
 <?php
 
-
-use App\Http\Actions\Animal\CreateAnimalAction;
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::get('/user', 'ShowUserListAction@run');
-
 
 Route::post('/animal','Animal\CreateAnimalAction');
