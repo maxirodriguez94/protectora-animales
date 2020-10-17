1aa7ae22ff39e183028dbf7c78c813e0de8473a2
maxi <maxi.rodriguez94@gmail.com>
2020-10-17 20:26:22 -0300

    Ruta y action carga persona

---
+++ app/Http/Actions/Persona/CreatePersonaAction.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Actions\Persona;
+
+
+use App\Persona;
+use Illuminate\Http\Request;
+
+
+class CreatePersonaAction
+{
+    public function __invoke(Request $request)
+    {
+        $persona = new Persona();
+        $persona->setNombre($request->nombre);
+        $persona->setDni($request->dni);
+        $persona->setDireccion($request->direccion);
+        $persona->setTelefono($request->telefono);
+        $persona->setLocalidad($request->localidad);
+        $persona->save();
+        return response()->json([
+            'status' => 'Ok',
+            'message' => 'Persona creada con exito',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', 'ShowUserListAction@run');
 
 Route::post('/animal','Animal\CreateAnimalAction');
+
+Route::post('/persona','Persona\CreatePersonaAction');
