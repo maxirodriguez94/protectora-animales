0c0618c622ec6ada248bcc376c6e0f189555300d
maxi <maxi.rodriguez94@gmail.com>
2020-11-06 19:09:16 -0300

    PUsh

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/user', 'ShowUserListAction@run');
 /*Animal */
 Route::post('/animal','Animal\CreateAnimalAction');
 Route::get('/animal/{id}/edit','Animal\EditAnimalAction');
-
+Route::get('/animal/list/adopcion', 'Animal\ListAnimalAdopcionAction');
 
 Route::post('/persona','Persona\CreatePersonaAction');
 
