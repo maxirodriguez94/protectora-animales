b050b7e309b2c0b3e94292fd941f61ab8faa2d44
Maxi Rodriguez <64332206+maxirodriguez94@users.noreply.github.com>
2020-11-07 18:57:57 -0300

    Action animal delete #9 #10 (#34)
    * Delete-animal
    
    * Action Delete animal, con retorno de json
    
    * ruta agregada
    
    * arreglo .env
    
    * arreglos
    
    * Areglo ruta linea 7
    
    Co-authored-by: MatiasGuazzaroni10 <matiasguazzaroni10@gmail.com>

---
+++ app/Http/Actions/Animal/DeleteAnimalAction.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Http\Actions\Animal;
+
+use App\Animal;
+
+class DeleteAnimalAction
+{
+    public function __invoke(int $id)
+    {
+        Animal::findOrFail($id)->delete();
+
+        return response()->json([
+            "status" => "Ok",
+            "message" => "El animal se elimino correctamente"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', 'ShowUserListAction@run');
-
 Route::post('/animal','Animal\CreateAnimalAction');
 Route::put('/animal/{id}/edit','Animal\EditAnimalAction');
+Route::get('/animal/delete','Animal\DeleteAnimalAction');
 Route::get('/animal/list/encontrados','Animal\ShowListFoundAnimalsAction');
 
 Route::post('/persona','Persona\CreatePersonaAction');
 Route::put('/persona/{id}/edit', 'Persona\EditPersonaAction');
+
+
 
