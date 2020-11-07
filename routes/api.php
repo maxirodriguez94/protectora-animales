0272eaa594ce5b4c38f04f365024a2300dfd457b
Nico Ferrero <nicolasferreronf5@gmail.com>
2020-11-07 19:09:32 -0300

    Action eliminar person 20 (#33)
    * Ruta y action carga animales
    
    * Borrar use no usado
    
    * Ruta y action carga persona
    
    * Espacio linea 21 y 23
    
    * 1
    
    * espacio linea 21-22
    
    * agreado de CreatePersonaActionTest
    
    * Ruta y action eliminar persona
    
    * arreglos
    
    * arreglos
    
    Co-authored-by: maxi <maxi.rodriguez94@gmail.com>
    Co-authored-by: cgilardi <cgilardi79@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,8 @@ Route::put('/animal/{id}/edit','Animal\EditAnimalAction');
 Route::get('/animal/delete','Animal\DeleteAnimalAction');
 Route::get('/animal/list/encontrados','Animal\ShowListFoundAnimalsAction');
 
-Route::post('/persona','Persona\CreatePersonaAction');
+Route::post('/persona/create','Persona\CreatePersonaAction');
+Route::delete('/persona/delete/{id}','Persona\DeletePersonaAction');
 Route::put('/persona/{id}/edit', 'Persona\EditPersonaAction');
 
 
