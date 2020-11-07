b15d5e79ef746d8a38733172e5c17cfee391e7c4
maxi <maxi.rodriguez94@gmail.com>
2020-11-07 18:50:56 -0300

    arreglos

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', 'ShowUserListAction@run');
+Route::post('/animal','Animal\CreateAnimalAction');
+Route::put('/animal/{id}/edit','Animal\EditAnimalAction');
+Route::get('/animal/delete','Animal/DeleteAnimalAction');
 
-
-Route::get('Animal/delete','DeleteAnimalAction');
+Route::post('/persona','Persona\CreatePersonaAction');
 
 
